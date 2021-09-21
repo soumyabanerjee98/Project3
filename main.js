b24b9a0e0b291351e110a3ec710270ad7c904582
soumyabanerjee98 <email>
2021-09-21 22:36:46 +0530

    Initial

---
+++ main.js
@@ -0,0 +1,33 @@
+// Clock section start
+const hourTime = document.querySelector('.time-hour');
+const minTime = document.querySelector('.time-min');
+const secTime = document.querySelector('.time-sec');
+const aMpM = document.querySelector('#am-time');
+
+function addZero(timeFrame) {
+    return timeFrame < 10 ? '0'.concat(timeFrame) : timeFrame
+}
+function time() {
+  var d = new Date();
+  var sec = d.getSeconds();
+  var min = d.getMinutes();
+  var hr = d.getHours();   
+     hourTime.innerHTML =    `${addZero((hr % 12) || 12)}`
+     minTime.innerHTML  =    `${addZero(min)}`
+     secTime.innerHTML  =    `${addZero(sec)}`
+     aMpM.innerHTML     =    `${hr >= 12 ? 'PM' : 'AM'}`
+}
+setInterval(time, 1000);
+// clock section end
+
+
+
+
+
+
+
+
+
+
+
+
